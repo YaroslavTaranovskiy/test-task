bab827368708d47195c3300ca166736baafb699c
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-10-07 21:51:39 +0300

    вва

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const params = {
+    API_KEY: '7c47f39dc565b202f275767eff95366e',
+    BASE_URL: 'https://api.themoviedb.org/3',
+};
+
+class MoviesInform {
+    constructor() {
+        this.
+    }
+}
